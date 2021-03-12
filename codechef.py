c560b797f792d55e551c5d78d4821756c6499ac4
Sumukh Bhardwaj <42494776+SumukhBhardwaj@users.noreply.github.com>
2021-03-12 22:44:59 +0530

    Added map to avoid multiple entries of a contest

--- codechef.py
+++ codechef.py
@@ -15,6 +15,10 @@ cnx = mysql.connector.connect(user='root', password='',
 
 mycursor = cnx.cursor()
 
+clear_table = "DELETE FROM CODECHEF";
+mycursor.execute(clear_table)
+cnx.commit()
+
 months = dict(Jan="01", Feb="02", Mar="03", Apr="04", May="05", Jun="06", Jul="07", Aug="08", Sep="09", Oct="10", Nov="11", Dec="12")
 
 url = "https://www.codechef.com/contests"
@@ -23,8 +27,9 @@ f3=open('events.txt','w')
 data=requests.get(url).text
 soup = BeautifulSoup(data,'lxml')
 
-cnt = 0
+map = dict() 
 
+cnt = 0
 for table in soup.findAll('table'):
  
  for row in table.findAll('tr'):
@@ -44,34 +49,23 @@ for table in soup.findAll('table'):
     
    cnt = cnt +1
 
-print(contest_code[1])
-print(contest_name[1])
+for i in range(len(contest_code)):
+  
+  if i == 0:
+   continue
+  
+  if contest_code[i] in map:
+   continue
+   
+  map[contest_code[i]] = 1
+  
+  start_time[i] = start_time[i][7:11]+'-'+months[start_time[i][3:6]]+'-'+start_time[i][0:2]+' '+start_time[i][-8:]
+  end_time[i] = end_time[i][7:11]+'-'+months[end_time[i][3:6]]+'-'+end_time[i][0:2]+' '+end_time[i][-8:]
+  
+  sql = "INSERT INTO codechef VALUES(%s, %s, %s, %s, %s)"
+  val = (contest_code[i], contest_name[i], start_time[i], 
+  	end_time[i], contest_url[i])
+  mycursor.execute(sql, val)
+  cnx.commit()
 
-start_time[1] = start_time[1][7:11]+'-'+months[start_time[1][3:6]]+'-'+start_time[1][0:2]+' '+start_time[1][-8:]
-end_time[1] = end_time[1][7:11]+'-'+months[end_time[1][3:6]]+'-'+end_time[1][0:2]+' '+end_time[1][-8:]
-
-print(start_time[1])
-print(end_time[1])
-print(contest_url[1])
-
-sql = "INSERT INTO codechef VALUES(%s, %s, %s, %s, %s)"
-val = (contest_code[1], contest_name[1], start_time[1], 
-	end_time[1], contest_url[1])
-
-mycursor.execute(sql, val)
-cnx.commit()
 cnx.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-# f3.write(column.text)
