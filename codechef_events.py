e2fb5dc236369195e091ffa4f75b6ef3a3ceef99
Sumukh Bhardwaj <sumukhbhardwaj21@gmail.com>
2021-03-29 00:35:58 +0530

    changed soup to selenium for dynamic-page scraping

--- codechef_events.py
+++ codechef_events.py
@@ -1,7 +1,6 @@
-import requests
-from datetime import datetime
-from bs4 import BeautifulSoup
 import mysql.connector
+from selenium import webdriver
+
 
 contest_code = []
 contest_name = []
@@ -23,55 +22,37 @@ months = dict(Jan="01", Feb="02", Mar="03", Apr="04", May="05", Jun="06", Jul="0
 
 url = "https://www.codechef.com/contests"
 
-data=requests.get(url).text
-soup = BeautifulSoup(data,'lxml')
+driver = webdriver.Chrome('C:/Users/HP/Desktop/chromedriver.exe')
+driver.get(url)
+driver.get("https://www.codechef.com/contests")
 
-print("1")
+tables = driver.find_elements_by_xpath("//table[contains(@class,'dataTable')]//tbody")
 
-map = dict() 
+for table in tables:
+    for row in table.find_elements_by_tag_name("tr"):
+        cnt = 0;
+        for details in row.find_elements_by_tag_name("td"):
+            if cnt == 0:
+                contest_code.append(details.text)
+                contest_url.append("https://www.codechef.com/"+details.text)
+            elif cnt == 1:
+                contest_name.append(details.text)
+            elif cnt == 2:
+                start_time.append(details.text)
+            else:
+                end_time.append(details.text)
+            cnt = cnt+1
 
-cnt = 0
-for table in soup.findAll('table'):
- 
- for row in table.findAll('tr'):
- 
-  cnt = 0
-  for column in row.findAll('td'):
-   
-   print(column.text)
-   if cnt == 0:
-    contest_code.append(column.text)
-    contest_url.append("https://www.codechef.com/"+column.text)
-   elif cnt == 1:
-    contest_name.append(column.text)
-   elif cnt == 2:
-    start_time.append(column.text)
-   elif cnt == 3:
-    end_time.append(column.text)
-    
-   cnt = cnt +1
-
-print(contest_url)
 
 for i in range(len(contest_code)):
-  
-  if i == 0:
-   continue
-  
-  if contest_code[i] in map:
-   continue
-   
-  map[contest_code[i]] = 1
-  
+
   start_time[i] = start_time[i][7:11]+'-'+months[start_time[i][3:6]]+'-'+start_time[i][0:2]+' '+start_time[i][-8:]
   end_time[i] = end_time[i][7:11]+'-'+months[end_time[i][3:6]]+'-'+end_time[i][0:2]+' '+end_time[i][-8:]
-  
+  print(contest_code[i])
   sql = "INSERT INTO codechef VALUES(%s, %s, %s, %s, %s)"
   val = (contest_code[i], contest_name[i], start_time[i], 
     end_time[i], contest_url[i])
   mycursor.execute(sql, val)
   cnx.commit()
-
-print("3")
 
 cnx.close()
