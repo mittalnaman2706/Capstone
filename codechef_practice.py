786cb9fad70b69dee2bf275e906ca03772b64ce9
Sumukh Bhardwaj <sumukhbhardwaj21@gmail.com>
2021-04-26 00:25:43 +0530

    added tags to practce problems and submissions in profile

--- codechef_practice.py
+++ codechef_practice.py
@@ -2,6 +2,7 @@ import requests
 from datetime import datetime
 from bs4 import BeautifulSoup
 import mysql.connector
+from selenium import webdriver
 
 cnx = mysql.connector.connect(user='root', password='',
                                  host='localhost',
@@ -9,9 +10,9 @@ cnx = mysql.connector.connect(user='root', password='',
 
 mycursor = cnx.cursor()
 
-clear_table = "DELETE FROM CODECHEF_PRACTICE";
-mycursor.execute(clear_table)
-cnx.commit()
+#clear_table = "DELETE FROM CODECHEF_PRACTICE";
+#mycursor.execute(clear_table)
+#cnx.commit()
 
 problem_type_array = ["school", "easy", "medium", "hard"]
 problem_type_name_array = ["Beginner", "Easy", "Medium", "Hard"]
@@ -38,7 +39,6 @@ for j in range(len(problem_type_name_array)):
       problem_name.append(column.text)
      elif cnt == 1:
       problem_code.append(column.text)
-      print(column.text)
       problem_url.append("https://www.codechef.com/problems/"+column.text)
       
      cnt = cnt +1
@@ -47,9 +47,21 @@ for j in range(len(problem_type_name_array)):
     
     if i == 0:
      continue
-    
-    sql = "INSERT INTO CODECHEF_PRACTICE VALUES(%s, %s, %s, %s)"
-    val = (problem_name[i], problem_code[i], problem_type_name_array[j], problem_url[i])
+
+    url = problem_url[i]
+
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument('headless')
+    driver = webdriver.Chrome('C:/Users/HP/Desktop/chromedriver.exe', chrome_options = chrome_options)
+    driver.get(url)
+    tags = ""
+    tables = driver.find_elements_by_xpath("//a[contains(@class,'problem-tag-small  mathjax-support')]")
+    for table in tables:
+        tags = tags + table.text + '$'
+    driver.close()
+
+    sql = "INSERT INTO CODECHEF_PRACTICE VALUES(%s, %s, %s, %s, %s)"
+    val = (problem_name[i], problem_code[i], problem_type_name_array[j], problem_url[i], tags)
 
     mycursor.execute(sql, val)
     cnx.commit()
