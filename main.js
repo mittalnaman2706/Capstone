cc48cb3381e62b7ddd109f7ee144d5d9bf92ce6a
Sumukh Bhardwaj <sumukhbhardwaj21@gmail.com>
2021-05-02 14:19:39 +0530

    added ejs modules

--- main.js
+++ main.js
@@ -8,7 +8,8 @@ const path = require('path');
 
 app.set("view options", {layout: false});
 app.use(express.static(__dirname + '/'));
-
+//app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
 var mysql = require('mysql');
 
 var con = mysql.createConnection(
@@ -41,25 +42,32 @@ app.use(session({
 }));
 
 
-app.get('/login', (req, res) => {
-  console.log("hello1")
-  var dataToSend;
-
-  const python = spawn('python', ['fetchContest.py']);
-  python.stdout.on('data', function (data) {
-  console.log('Pipe data from python script ...');
-  dataToSend = data.toString();
-  });
-
-  python.on('close', (code) => {
-  console.log(`child process close all stdio with code ${code}`);
-  res.send(dataToSend)
+app.get('/CodersGym', (req, res) => {
+  res.render(__dirname +'/Frontend/Landing Page-Final/cards.html')
+  //res.send(dataToSend)
 
   });
-});
+
+app.get('/UpcomingContests', (req, res) => {
+    var dataToSend;
+
+    const python = spawn('python', ['fetchContest.py']);
+    python.stdout.on('data', function (data) {
+    console.log('Pipe data from python script ...');
+    dataToSend = data.toString();
+    });
+
+    python.on('close', (code) => {
+    console.log(`child process close all stdio with code ${code}`);
+    console.log(dataToSend)
+    console.log(dataToSend[0])
+    res.render(__dirname +'/Frontend/Problem+Challenges-Final/FinalChallengesModule',{testing: "hello"})
+  });
+  });
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.post('/login', (req, res) => {
  console.log("hello")
