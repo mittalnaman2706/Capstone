0ca8610dc798a5f814d5c1e286f977abfbb3a212
Sumukh Bhardwaj <sumukhbhardwaj21@gmail.com>
2021-06-03 00:42:59 +0530

    final commit goodbye b.tech

--- Frontend/Profile Analyser/pdf.js
+++ Frontend/Profile Analyser/pdf.js
@@ -4,9 +4,13 @@ window.onload = function () {
             const invoice = this.document.getElementById("invoice");
             console.log(invoice);
             console.log(window);
+
+            var script_tag = document.getElementById('pdf')
+            var username = script_tag.getAttribute("username") + "-Report"
+
             var opt = {
                 margin: 2,
-                filename: 'user_profile.pdf',
+                filename: username,
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
                 jsPDF: { unit: 'in', format: 'tabloid', orientation: 'landscape' }
