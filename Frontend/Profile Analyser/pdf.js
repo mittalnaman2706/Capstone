b705e0bfd1419f01e4e3367f010cb6b81cc8ae7f
Sumukh Bhardwaj <sumukhbhardwaj21@gmail.com>
2021-06-02 18:34:54 +0530

    added pdf generator

--- Frontend/Profile Analyser/pdf.js
+++ Frontend/Profile Analyser/pdf.js
@@ -5,11 +5,11 @@ window.onload = function () {
             console.log(invoice);
             console.log(window);
             var opt = {
-                margin: 1,
+                margin: 2,
                 filename: 'user_profile.pdf',
                 image: { type: 'jpeg', quality: 0.98 },
                 html2canvas: { scale: 2 },
-                jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' }
+                jsPDF: { unit: 'in', format: 'tabloid', orientation: 'landscape' }
             };
             html2pdf().from(invoice).set(opt).save();
         })
